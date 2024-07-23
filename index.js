8db8b16b44e63aee3f61b47001388d3c24bdc12d
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-07-23 12:31:40 +0530

    changed the project view

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    const projectVideos = document.querySelectorAll('.project .video video');
+    for (const video of projectVideos) {
+        video.addEventListener("mouseenter",(e) => {
+            e.target.play();
+        });
+    
+        video.addEventListener("mouseleave",(e) => {
+            e.target.pause();
+            e.target.currentTime = 0;
+        });
+    }
+
     // Smooth scroll
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -41,6 +53,70 @@ document.addEventListener('DOMContentLoaded', function() {
           });
         });
     });
+
+    var options = {
+        strings: ["Hello, I'm a web developer.", "I love coding.", "Welcome to my portfolio!"],
+        typeSpeed: 45,
+        backSpeed: 25,
+        startDelay: 0,
+        loop: true
+    };
+  
+    var typed = new Typed("#my-titles", options);
+
+    const aboutSection = document.getElementById('about');
+    const moreLink = document.getElementById('more-link');
+    let collapsed = true;
+    moreLink.addEventListener('click', e => {
+        if(collapsed){
+            aboutSection.querySelector('.para').classList.remove('max-h-[200px]');
+            aboutSection.querySelector('.para div').classList.remove('hidden');
+            moreLink.innerText = 'See less...';
+        }
+        else{
+            aboutSection.querySelector('.para').classList.add('max-h-[200px]');
+            aboutSection.querySelector('.para div').classList.add('hidden');
+            moreLink.innerText = 'See more...';
+        }
+
+        collapsed = !collapsed;
+    });
+
+    //Project animations
+    const projectContainer = document.getElementById('projects');
+    const projectOverviewContainer = document.querySelector('#projects .overview-container');
+    const projects = document.querySelectorAll('#projects .project');
+
+    let overviewInUse = false, firstTime = true;
+    for (const project of projects) {
+
+        project.addEventListener('click', e => {
+            overviewInUse = true;
+            if(! projectOverviewContainer.classList.contains('show')){
+                projectOverviewContainer.classList.add('show');
+            }
+            project.classList.add('active');
+            setTimeout(() => {
+                projectOverviewContainer.classList.add('content');
+            }, 500);
+            project.focus();
+            projectContainer.scrollIntoView();
+        });
+
+        project.addEventListener('focusout', e => {
+            projectOverviewContainer.classList.remove('content');
+            setTimeout(() => {
+                project.classList.remove('active');
+
+                if(!overviewInUse || firstTime){
+                    projectOverviewContainer.classList.remove('show');
+                }
+
+                overviewInUse = false;
+                firstTime = false;
+            }, 500);
+        })
+    }
 });
 
 function getElementFromIdLink(idLink = ""){
